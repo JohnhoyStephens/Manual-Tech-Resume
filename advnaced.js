b8c05c85f96df5d1c2d13010174462e3d1d6c6d0
Johnhoy Stephens <61854815+JohnhoyStephens@users.noreply.github.com>
2022-08-01 13:44:19 -0500

    Updaing doc for desktop usgage

---
+++ advnaced.js
@@ -0,0 +1,12 @@
+//let /const
+const player = "bobby";
+let expericne = 1000;
+let wizard = false;
+
+// let when wrapped around a curly brace is is create a new instnace/var
+
+// dont use var and more use let
+
+// const for var that donesnthcnage
+const { player, expericne } = obj;
+a;
